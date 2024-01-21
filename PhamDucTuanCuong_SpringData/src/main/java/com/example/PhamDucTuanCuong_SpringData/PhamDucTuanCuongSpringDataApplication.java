6919bf750c25237405bc171b0c8830c596c47947
Cuong <eticon0685@gmail.com>
2024-01-21 20:52:02 +0700

    1

---
+++ PhamDucTuanCuong_SpringData/src/main/java/com/example/PhamDucTuanCuong_SpringData/PhamDucTuanCuongSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.PhamDucTuanCuong_SpringData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamDucTuanCuongSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamDucTuanCuongSpringDataApplication.class, args);
+	}
+
+}
---
+++ PhamDucTuanCuong_SpringData/src/test/java/com/example/PhamDucTuanCuong_SpringData/PhamDucTuanCuongSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PhamDucTuanCuong_SpringData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamDucTuanCuongSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
